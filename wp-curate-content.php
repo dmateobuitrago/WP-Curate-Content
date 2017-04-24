8e767e06332dc91540ab35bcfc36abae1afc6f97
Mateo Buitrago <dmateobuitrago@gmail.com>
2017-04-23 22:49:54 -0500

    chore: setup plugin

---
+++ wp-curate-content.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: Curate Content
+Plugin URI:  https://pappcorn.com
+Description: Allow users to save content from other websites
+Version:     1.0
+Author:      Mateo Buitrago
+Author URI:  https://github.com/dmateobuitrago
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: wp-curate-content
+*/
+
+if( ! defined ('ABSPATH')){
+    exit();
+}
+
+?>
